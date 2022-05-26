ebec8e90c128edfd00e9577618d24b931842b7a8
manisha <manishashete15@gmail.com>
2022-05-26 15:22:49 +0530

    modified jwtmiddleware file

--- Backend/helpers/jwtMiddleware.js
+++ Backend/helpers/jwtMiddleware.js
@@ -1,31 +1,32 @@
-const { expressjwt: expressJwt } = require("express-jwt");
-const authJwt = () => {
+const expressJwt = require("express-jwt");
+
+function authJwt() {
   const secret = process.env.SECRETKEY;
   return expressJwt({
     secret: secret,
     algorithms: ["HS256"],
-    // isRevoked: isRevoked,
+    isRevoked: isRevoked,
   }).unless({
     path: [
       { url: /\/public\/uploads(.*)/, methods: ["GET", "OPTIONS"] },
-      { url: /\/product(.*)/, methods: ["GET", "OPTIONS", "POST"] },
+      { url: /\/product(.*)/, methods: ["GET", "OPTIONS"] },
       { url: /\/categories(.*)/, methods: ["GET", "OPTIONS"] },
       {
         url: /\/order(.*)/,
-        methods: ["GET", "OPTIONS", "POST", "PUT", "DELETE"],
+        methods: ["GET", "OPTIONS", "POST"],
       },
       "/user/login",
       "/user/register",
     ],
   });
-};
+}
 
-// async function isRevoked(req, payload, done) {
-//   console.log(payload.isAdmin);
-//   if (!payload.isAdmin) {
-//     done(null, true);
-//   }
-//   done();
-// }
+async function isRevoked(req, payload, done) {
+  if (payload.isAdmin == true) {
+    done();
+  } else {
+    done(null, true);
+  }
+}
 
 module.exports = authJwt;
