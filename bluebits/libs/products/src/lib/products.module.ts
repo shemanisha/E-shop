d2f34852ecae3ade4e7cb89d1d6c135c3a1a98ec
shemanisha <96568158+shemanisha@users.noreply.github.com>
2022-07-20 14:29:22 +0530

    created category-banner and styling

--- bluebits/libs/products/src/lib/products.module.ts
+++ bluebits/libs/products/src/lib/products.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductSearchComponent } from './components/product-search/product-search.component';
+import { CategoriesBannerComponent } from './components/categories-banner/categories-banner.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ProductSearchComponent],
-  exports: [ProductSearchComponent],
+  declarations: [ProductSearchComponent, CategoriesBannerComponent],
+  exports: [ProductSearchComponent, CategoriesBannerComponent],
 })
 export class ProductsModule {}
