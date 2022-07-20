fdb8a39587159114ee5d24f78ebb26a5a3e815a0
shemanisha <96568158+shemanisha@users.noreply.github.com>
2022-07-20 18:47:49 +0530

    created footer component and added styling for same

--- bluebits/apps/ngshop/src/app/shared/footer/footer.component.ts
+++ bluebits/apps/ngshop/src/app/shared/footer/footer.component.ts
@@ -5,7 +5,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'ngshop-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
   constructor() {}
